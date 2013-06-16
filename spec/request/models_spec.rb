2ba3e140d92a9fd10022f665d95671da3e275f7e
Luis Bacelar <lmbacelar@gmail.com>
2013-06-16 01:21:41 +0100

    added fixture data, and spec and view for models list

--- spec/request/models_spec.rb
+++ spec/request/models_spec.rb
@@ -1,17 +1,21 @@
 require 'spec_helper'
 
-describe 'Model Page' do
-  describe 'GET /model' do
+describe 'Models Page' do
+  describe 'GET /models' do
+    before(:each) { visit '/models' }
+
     context 'empty database' do
-      before(:each) { visit '/model' }
       it 'renders success' do
         expect(page.status_code).to be 200
       end
     end
+
     context 'populated database' do
+      fixtures :models
+
       it 'show a list of models' do
-        pending 'Need to write model and its specs'
-        expect(page).to have_selector 'li a', text: 'name'
+        expect(page).to have_selector 'dt a', text: 'PC.40602'
+        expect(page).to have_selector 'dd',   text: 'piston gauge'
       end
     end
   end
