53935e807a3a136edd8ae8f1fa1523154b3160e9
Luis Bacelar <lmbacelar@gmail.com>
2013-06-16 01:20:15 +0100

    added recent_models helper method

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -1,6 +1,13 @@
-class ModelController < ApplicationController
+class ModelsController < ApplicationController
+
+  helper_method :recent_models
 
   def index
   end
 
+  protected
+
+  def recent_models
+    Model.recent
+  end
 end
