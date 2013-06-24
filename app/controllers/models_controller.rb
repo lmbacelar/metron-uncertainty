d351cc2393d9980399b7832329564eab4b469c7c
Luis Bacelar <lmbacelar@gmail.com>
2013-06-24 12:14:00 +0100

    started implementation of show model page

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -1,13 +1,24 @@
 class ModelsController < ApplicationController
 
-  helper_method :recent_models
+  helper_method :model, :recent_models, :variables
 
   def index
   end
 
+  def show
+  end
+
   protected
+
+  def model
+    Model.find_by! url: params[:id]
+  end
 
   def recent_models
     Model.recent
+  end
+
+  def variables
+    model.variables
   end
 end
--- spec/request/models_spec.rb
+++ spec/request/models_spec.rb
@@ -20,3 +20,31 @@ describe 'Models Page' do
     end
   end
 end
+
+describe 'Models detail' do
+  describe 'GET /models/:id' do
+    fixtures :models
+    before(:each) { visit '/models/pc-dot-40602' }
+
+    it 'renders success' do
+      expect(page.status_code).to be 200
+    end
+
+    describe 'individual model' do
+      it 'displays name' do
+        expect(page).to have_selector 'h2', text: 'PC.40602'
+      end
+
+      it 'displays description' do
+        expect(page).to have_selector 'div.model dl dt', text: 'Description'
+        expect(page).to have_selector 'div.model dl dd', text: 'piston gauge'
+      end
+
+      it 'displays equation with links to variables' do
+        pending 'inplement helper method to repalce X$(...) by link_to variable'
+        expect(page).to have_selector 'div.model dl dt', text: 'Equation'
+      end
+    end
+
+  end
+end
